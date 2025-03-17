776d7fdff9d662c38b80aa6780ad140bcaebb567
Henrique C Lucas <henrique.c.lucas@hotmail.com>
2025-03-17 16:55:27 -0300

    terminando as aulas com o devsoutinho

---
+++ 03-minibrowser/map.js
@@ -0,0 +1,17 @@
+// TDD - Test Driven Development
+
+var novoMapa = new Map();
+
+class HistoryMap{
+    
+}
+
+novoMapa.set('11/11/11 - 23:30 - URL',{title: 'titulo do site', url: "url sozinha"})
+novoMapa.set('11/11/11 - 23:32 - URL',{})
+novoMapa.set('11/11/11 - 23:32 - URL',{})
+
+
+
+
+var resultado = novoMapa;
+console.log(resultado);
