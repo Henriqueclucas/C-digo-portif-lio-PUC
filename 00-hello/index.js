78bbdfd116eaff8cdfee0981791ec3f8f705c7d8
Henrique <henrique.c.lucas@hotmail.com>
2025-03-20 19:09:12 -0300

    Att

---
+++ 00-hello/index.js
@@ -0,0 +1,56 @@
+/*function avaliaParidade(limiteSuperior){
+    for(let i =0; i<limiteSuperior; i++){
+        if(i%2 == 1)
+            console.log(i+ " é um numero impar");
+        else{
+            console.log(i+ " é um numero par");
+        }
+    }
+
+}
+
+avaliaParidade(10);
+
+
+ var mySet = new Set();
+
+ mySet.add(0);
+ console.log(mySet);
+ console.log('--------');
+
+ mySet.add(1);
+ console.log(mySet);
+ console.log('--------');
+
+ mySet.add(1);
+ console.log(mySet);
+ console.log('--------');
+
+
+ mySet.add(2);
+ console.log(mySet);
+ console.log('--------');
+
+ mySet.add(3);
+ console.log(mySet);
+ console.log('--------');
+
+ mySet.add(2);
+ console.log(mySet);
+ console.log('--------');
+
+ const contatos = new Map();
+
+ contatos.set('Jessie', {phone: '5479-7463', address: 'rua 4'});
+ contatos.has('Jessie'); // true
+ contatos.has('Hilary') // undefined
+ 
+ var x =0;
+ var n=0;
+ while(n<3){
+    n=n+1;
+    x=n+x;
+ }
+ console.log(x);*/
+
+ 
